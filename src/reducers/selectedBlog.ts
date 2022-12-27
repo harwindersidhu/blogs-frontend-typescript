e084e249a061a1bdd434c7157e4d378a53340f16
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-12-27 13:13:12 -0700

    Refactored the emmpty response issue

--- src/reducers/selectedBlog.ts
+++ src/reducers/selectedBlog.ts
@@ -5,7 +5,7 @@ const defaultValue = {
   id : 1,
   title: "",
   slug: "",
-  content: "<h2>Unable to find Blog. Please check the slug value again.</h2>",
+  content: "",
   image: "",
   published_at: ""
 }
@@ -13,7 +13,7 @@ const defaultValue = {
 export const selectedBlogReducer = (state: Blog = defaultValue, action: GetBlogBySlugAction) => {
   switch (action.type) {
     case ActionTypes.getBlogBySlug:
-      return action.payload || defaultValue;
+      return action.payload || {...defaultValue, content: "<h2>Unable to find a Blog. Please check the slug value and try again.</h2>"};
     default:
       return state;
   }
